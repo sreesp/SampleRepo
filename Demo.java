78e8c26ecf0f281420be43ef583549cc73e6f9bc
sreesp <sumaheswararao@gmail.com>
2023-08-07 15:13:46 +0530

    Changes made by Pranav Developer

--- Demo.java
+++ Demo.java
@@ -3,5 +3,7 @@ class Demo
 	public static void main(String args[])
 	{
 		System.out.println("Hello World!");
+		System.out.println("Welcome to Java");
+		System.out.println("Modified by Dev: Pranav");
 	}
 }
